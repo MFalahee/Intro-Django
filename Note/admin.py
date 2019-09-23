2c9dd0e6bbb7fc7ad7e6f0d2c4327e5829602a32
mjfalahee <mjfalahee@gmail.com>
2019-09-23 12:06:31 -0700

    Whirled through day2 of intro, on day3 now

--- Note/admin.py
+++ Note/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Note
+
+#this class is to get read-only fields to show up in the interface
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields=('created_at', 'last_modified')
 
 # Register your models here.
+admin.site.register(Note, NoteAdmin)
+
+#if you want to register more models you do so with additional register() calls.
--- Note/models.py
+++ Note/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 
 class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+    #user edited fields
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    #timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
 
+
+class PersonalNote(Note): #this new class inherits Note (and all of its fields)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #FK to User
