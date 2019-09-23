029da7ccb7807b15d42944a3ea85d28f819c1f88
mjfalahee <mjfalahee@gmail.com>
2019-09-23 12:25:10 -0700

    Got things being written to the server

--- Notes/urls.py
+++ Notes/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from Note.api import PersonalNoteViewSet 
+
+router = routers.DefaultRouter()
+
+#We can use router.register to add as many paths as we want when we include it in url patterns in this way.
+router.register(r'notes', PersonalNoteViewSet)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls))
 ]
+
