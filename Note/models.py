c68a577ed720a3724f733b7d1893e8bee8b0fc03
mjfalahee <mjfalahee@gmail.com>
2019-09-23 11:14:58 -0700

    new key and tested models with Note

--- Note/models.py
+++ Note/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
 
@@ -6,4 +7,5 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
 
-    
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
